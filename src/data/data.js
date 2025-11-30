24925c29aff9419fcc17b7c4ebbe778201d35871
Emanuel Bellanti <bellantiemanuel@gmail.com>
2025-11-30 18:52:40 -0300

    llaves públicas corregidas

--- src/data/data.js
+++ src/data/data.js
@@ -1,25 +1,21 @@
 import 'dotenv/config';
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
 
-import { getFirestore } from "firebase/firestore";
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA4RAv0p7ErSvhGSr1k7zBlyeXRDvbqIPo",
-  authDomain: "backend-nodejs-23bad.firebaseapp.com",
-  projectId: "backend-nodejs-23bad",
-  storageBucket: "backend-nodejs-23bad.firebasestorage.app",
-  messagingSenderId: "24459820329",
-  appId: "1:24459820329:web:27ae16c39fea3aee948b38"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: "backend-nodejs-23bad.firebasestorage.app",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: "24459820329",
+    appId: process.env.FIREBASE_APP_ID
 };
 
-// Initialize Firebase
+  // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+  // Initialize Firestore
 const db = getFirestore(app);
 
 export { db };
